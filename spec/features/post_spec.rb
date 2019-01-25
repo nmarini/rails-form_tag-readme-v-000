c62c16209e7f4c06633c9382d7db092de04986cd
Nicolas Marini <nmarini44@gmail.com>
2019-01-25 00:47:19 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -8,6 +8,6 @@ describe 'new post' do
 
   it 'renders HTML in the /new template' do
     visit new_post_path
-    expect9page.to have_content('Post Form')
+    expect(page).to have_content('Post Form')
   end
 end
